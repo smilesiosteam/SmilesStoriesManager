a8598b5dd65fc3347c78747b50b6fa331b8e83c6
Muhammad Shayan Zahid <shahyan.zahid@systemsltd.com>
2023-09-04 16:47:44 +0500

    stories table view cell, stories collection view cell added

---
+++ Sources/SmilesStoriesManager/View/CustomViews/StoriesTVC/StoriesCVC/StoriesCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  StoriesCollectionViewCell.swift
+//  House
+//
+//  Created by Shahroze Zaheer on 10/26/22.
+//  Copyright © 2022 Ahmed samir ali. All rights reserved.
+//
+
+import UIKit
+
+public class StoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var imgOverlay: StoriesSZGradientView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        setupUI()
+    }
+    
+    func setupUI() {
+        title.font = .circularXXTTMediumFont(size: 14)
+        title.textColor = .appRevampLocationTextColor
+        
+        descriptionLabel.font = .circularXXTTBookFont(size: 12)
+        descriptionLabel.textColor = .appRevampSubtitleColor
+        //        imgOverlay.showGradientColor(with: [UIColor.black.withAlphaComponent(0.3).cgColor,
+        //                                            UIColor.black.withAlphaComponent(0).cgColor], for: "bottom")
+        //        imgOverlay.addMaskedCorner(withMaskedCorner: [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner], cornerRadius: 8.0)
+        imgOverlay.locations = [0.25, 0.54, 0.83]
+        imgOverlay.colors = [
+            UIColor.black.withAlphaComponent(0.0).cgColor,
+            UIColor.black.withAlphaComponent(0.2).cgColor,
+            UIColor.black.withAlphaComponent(0.85).cgColor
+        ]
+        imgOverlay.direction = 2
+        imgOverlay.layer.cornerRadius = 8
+        imgOverlay.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        icon.addBorder(withBorderWidth: 1, borderColor: .white)
+        
+        image.addMaskedCorner(withMaskedCorner: [.layerMinXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner], cornerRadius: 8.0)
+        icon.addMaskedCorner(withMaskedCorner: [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner], cornerRadius: icon.frame.height / 2)
+    }
+    
+    public func configure(story: Story) {
+        image.setImageWithUrlString(story.imageUrl.asStringOrEmpty()) { image in
+            if let image = image {
+                self.image.image = image
+            }
+        }
+        
+        icon.setImageWithUrlString(story.logoUrl.asStringOrEmpty()) { image in
+            if let image = image {
+                self.icon.image = image
+            }
+        }
+        
+        title.text = story.title
+        descriptionLabel.text = story.storyDescription
+    }
+}
---
+++ Sources/SmilesStoriesManager/View/CustomViews/StoriesTVC/StoriesTableViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  StoriesTableViewCell.swift
+//  House
+//
+//  Created by Shahroze Zaheer on 10/26/22.
+//  Copyright © 2022 Ahmed samir ali. All rights reserved.
+//
+
+import UIKit
+
+public class StoriesTableViewCell: UITableViewCell {
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    public var collectionsData: [Story]?{
+        didSet{
+            self.collectionView?.reloadData()
+        }
+    }
+    public var callBack: ((Story) -> ())?
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        collectionView.register(UINib(nibName: String(describing: StoriesCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: StoriesCollectionViewCell.self))
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = setupCollectionViewLayout()
+    }
+    
+    public override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func setupCollectionViewLayout() ->  UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+            
+            let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 12)
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .absolute(116), heightDimension: .absolute(250)), subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.contentInsets.leading = 16
+            return section
+        }
+        
+        return layout
+    }
+    
+    public func setBackGroundColor(color: UIColor) {
+        mainView.backgroundColor = color
+    }
+}
+
+extension StoriesTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionsData?.count ?? 0
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let story = collectionsData?[indexPath.row] {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StoriesCollectionViewCell", for: indexPath) as? StoriesCollectionViewCell else {return UICollectionViewCell()}
+            cell.configure(story: story)
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let data = collectionsData?[indexPath.row] {
+            callBack?(data)
+        }
+    }
+}
